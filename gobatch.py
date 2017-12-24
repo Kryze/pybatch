7aaf5cb27131506c57060c97d21d07425230db65
claurene <laur.cladt@gmail.com>
2017-12-24 15:51:33 +0100

    Gestion des paramètres et ajout des files de messages

--- gobatch.py
+++ gobatch.py
@@ -11,24 +11,47 @@ import posix_ipc as pos
 
 #TODO Mettre le repertoire avec le truc comme HOME a la place de benjamin
 def lectureFichier():
-        #On ouvre le fichier fbatch.txt en lecture (r)
-        with open(os.path.expanduser("~/fbatch.txt"),"r") as f:
-            #On retire les \r\n du fichier pour éviter leurs affichage lors de la lecture
-            f=f.read().splitlines()
-            #Pour chaque ligne
-            for line in f:
-                #On affiche la ligne
-                # A faire Plus tard, afficher le PID et vérifier qu'il tourne encore, sinon le relancer pour chaque ligne
-                print(line)
+    #On ouvre le fichier fbatch.txt en lecture (r)
+    with open(os.path.expanduser("~/fbatch.txt"),"r") as f:
+        #On retire les \r\n du fichier pour éviter leurs affichage lors de la lecture
+        f=f.read().splitlines()
+        #Pour chaque ligne
+        for line in f:
+            #On affiche la ligne
+            # A faire Plus tard, afficher le PID et vérifier qu'il tourne encore, sinon le relancer pour chaque ligne
+            print(line)
 
+def ecritureFichier(msg):
+    #On ouvre le fichier fbatch.txt en écriture append pour ajouter et non pas remplacer (a)
+    with open(os.path.expanduser("~/fbatch.txt"),"a") as f:
+        print("gobatch: Ecriture de la commande")
+        f.write(msg+" \r\n")
 
 def run():
     while True:
         print("gobatch: En attente d'une commande")
         #Le processus est bloqué tant qu'aucune commande pgcycl n'est arrivée
         S.acquire()
-        lectureFichier()
+        #lectureFichier()
+        try:
+            filmess=pos.MessageQueue("/queue",pos.O_CREAT) # ouvre la file
+        except pos.ExistentialError:
+            pos.unlink_message_queue("/queue") # détruit la file
+            filmess = pos.MessageQueue("/queue",pos.O_CREAT) # puis redemande la création
+        (message,priorite)=filmess.receive() # retire le message en tête de file
+        print("gobatch : Le message reçu est : {}, de priorité {}".format(message,priorite))
 
+        # Type d'action en fonction de la priorité (type) du message
+        if priorite == 1:
+            # Action : lecture du fichier fbatch
+            lectureFichier()
+        elif priorite == 2:
+            # Action : suppression de la commande spécifiée
+            print ""
+        elif priorite == 3:
+            # Action : écriture dans le fichier fbatch
+            ecritureFichier(message)
+        
 if __name__ == "__main__":
     #Initialisation du sémaphore
     try:
--- pgcycl.py
+++ pgcycl.py
@@ -8,16 +8,64 @@ import os
 import sys
 import time
 import posix_ipc as pos
+import re
 
+def run():    
+    # pgcycl envoie un message à gobatch. La priorité change en fonction du paramètre.
+    
+    # Création de la file de messages
+    try:
+        filmess = pos.MessageQueue("/queue",pos.O_CREAT)
+        print("pgcycl : creation de la file de message")
+    except pos.ExistentialError:
+        pos.unlink_message_queue("/queue") # détruit la file
+        filmess = pos.MessageQueue("/queue",pos.O_CREAT) # puis redemande la création
+    
+    # On récupère les arguments
+    args=sys.argv
 
-def pgcyclwrite():
-    #On ouvre le fichier fbatch.txt en écriture append pour ajouter et non pas remplacer (a)
-    with open(os.path.expanduser("~/fbatch.txt"),"a") as f:
-        print("pgcycl: Ecriture de la commande")
-        #sys.argv contient tous les arguments dans un tableau, ici on veut une ligne avec la commande
-        for arg in sys.argv:
-            f.write(arg+" ")
-        f.write("\r\n")
+    # On vérifie le paramètre
+    if re.match("^-[lda]$",args[1]):
+        param=args[1][1]
+
+        # Actions à effectuer en fonction du paramètre
+        if param=="l":
+            # Affichage du fichier fbatch
+            message="placeholder L"
+            filmess.send(message,None,1)
+            print "pgcycl : message {} envoyé".format(message)
+        elif param=="d":
+            # Suppression d'une ligne
+            message="placeholder D"
+            filmess.send(message,None,2)
+            print "pgcycl : message {} envoyé".format(message)
+        elif param=="a":
+            # Ajout d'une ligne au fichier fbatch
+            # On récupère les arguments correspondant au temps
+            cron=[]
+            try:
+                for arg in args[2:-3]: 
+                    if re.match("^[0-9]{1,2}$",arg):
+                        cron.append(int(arg)) # Sous forme numérique pour tester la validité (à voir)
+                    elif re.match("^\*$",arg):
+                        cron.append("*")
+                    else:
+                        print "Paramètre {} invalide.".format(arg)
+                # On récupère le nom de la commande ainsi que les fichiers de sortie
+                cmd=args[-3]
+                stdout=args[-2]
+                stderr=args[-1]
+
+                # On envoie le message à gobatch : ce qu'il devra écrire dans fbatch
+                #TODO: Format du message avec les infos ci-dessus
+                message="placeholder A"
+                filmess.send(message,None,3)
+                print "pgcycl : message {} envoyé".format(message)
+            except IndexError:
+                print "Nombre de paramètres invalides."
+    else:
+        print "Paramètre invalide. Utilisez -l, -d ou -a"
+    
 
 if __name__ == "__main__":
     #Initialisation du sémaphore
@@ -26,7 +74,7 @@ if __name__ == "__main__":
     except pos.ExistentialError:
         S = pos.Semaphore("/S1",pos.O_CREAT)
     print(str(sys.argv))
-    pgcyclwrite()
-    #On relache le sémaphore après avoir écrit une nouvelle ligne de commande dans le fichier
+    run()
+    #On relache le sémaphore après avoir envoyé un message à gobatch
     S.release()
     print("pgcycl: Relachement du semaphore")
